b179350fd8221694c239393d519aa0b70a441c59
baccy_drm <baccy_drm@dce8e5b7-fb20-0410-bec9-edb67563d200>
2007-01-13 20:13:13 +0000

    new: added BadVista and DefectiveByDesign banner, subpages and links are now configured by a simple array

--- homepage/trunk/home/index.php
+++ homepage/trunk/home/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function ShowTD()
 {
 	print("background=\"home/prost.gif\"");
@@ -9,6 +10,7 @@ function ShowTD()
 function ShowPage()
 {
 	global $BERLIOS_GROUP_ID;
+	global $subpages;
 
 
 	// link to the news headers (including some lines)
@@ -19,52 +21,24 @@ function ShowPage()
 	print("<p><br>\n");
 
 
-	print("<h3>CBMImager</h3>\n");
-	print("CBMImager is a tool to create and edit disk images for the Commodore C64 and compatible computers.\n");
-	print("It supports full read/write access to D64 and DFI and read-only access to images of a CMD HD.<br>\n");
-	print("<a href=\"index.php?area=cbmimager\">[More...]</a>\n");
-	print("<p><br>\n");
-
-	print("<h3>DLoad</h3>\n");
-	print("DreamLoad (or short DLoad) is a 2 bit IRQ fastloader for many different drives like 1541 and compatible,\n");
-	print("1570/71, 1581 and the CMD devices. The latest version also supports the MMC64.\n");
-	print("DLoad works works on pal and ntsc machines and can easily be combined with crunchers and levelpackers.<br>\n");
-	print("<a href=\"index.php?area=dload\">[More...]</a>\n");
-	print("<p><br>\n");
-
-	print("<h3>DreamAss</h3>\n");
-	print("DreamAss is a crossassembler. It produces binaries for 6510, 65816 and compatible CPUs.\n");
-	print("The 'illegal' opcodes of the 6510 are also supported.<br>\n");
-	print("<a href=\"index.php?area=dreamass\">[More...]</a>\n");
-	print("<p><br>\n");
-
-	print("<h3>DreaMon</h3>\n");
-	print("DreaMon is an assembly language monitor for the SCPU. It requires a Super Ram Card. It supports Freezepoints and\n");
-	print("has a very comfortable and intuitive scroll mode for debugging. DreaMon can use the normal Vic display or on a C128\n");
-	print("the 80 columns screen with up to 50 lines (depends on the monitor)<br>\n");
-	print("<a href=\"index.php?area=dreamon\">[More...]</a>\n");
-	print("<p><br>\n");
-
-	print("<h3>DreaMon_crt</h3>\n");
-	print("DreaMon_crt is a port of the DreaMon project to the Retro Replay cart.<br>\n");
-	print("<a href=\"index.php?area=dreamon_crt\">[More...]</a>\n");
-	print("<p><br>\n");
-
-	print("<h3>MMC64 Plugin</h3>\n");
-	print("This project provides a framework for MMC64 plugins. It contains several examples like a D64 and Lynx extractor and the DFI plugin.<br>\n");
-	print("<a href=\"index.php?area=mmc64_plugin\">[More...]</a>\n");
-	print("<p><br>\n");
-
-	print("<h3>Mylene</h3>\n");
-	print("Mylene is a file browser library for the C64. It can be configured to display various information and supports caching.\n");
-	print("Mylene is used in the dfi and lnx plugins for the MMC64.<br>\n");
-	print("<a href=\"index.php?area=mylene\">[More...]</a>\n");
-	print("<p><br>\n");
+	foreach($subpages as $section=>$ar_attr)
+	{
+		// is this the 'home' page?
+		if( $section!='home' )
+		{
+			print '<h3>';
+			print $ar_attr['name'];
+			print "</h3>\n";
+			print $ar_attr['dsc'];
+			print "<br><a href=\"index.php?area=$section\">[More...]</a>\n";
+			print "<p><br>\n";
+		}
+	}
 
 	print("This page is best viewed with <a href=\"http://elinks.cz\">links</a> in 80x50. But then you would miss Uncle Tom and TWOO in the background (I guess everything has it's ups and downs).\n");
 	print("<br>IIRC Tom was yelling 'Beeer!'. Or was it 'Dreeeams!' ? Hum, nevermind... almost the same anyway. ;)\n");
 	print("<p><br>\n");
-	print("Crappy page design by <a href=\"mailto:baccy_drm@berlios.de\">Doc Bacardi</a>/The Dreams\n");
+	print("Crappy page design by Doc Bacardi/The Dreams\n");
 	print("<p><br>\n");
 }
 
--- homepage/trunk/index.php
+++ homepage/trunk/index.php
@@ -1,8 +1,82 @@
 <?php
 
-// Global Vars
+// this is the configuration
+
+
+// area name, page path and page description
+// NOTE: the first entry in this array must be the main page. it is omitted in the projects listing
+$subpages = array(
+	'home'		=> array(name=>'Home', 'path'=>'home/index.php', 'dsc'=>
+''),
+
+	'cbmimager'	=> array(name=>'CBMImager', 'path'=>'cbmimager/index.php', 'dsc'=>
+'CBMImager is a tool to create and edit disk images for the Commodore C64 and compatible computers.
+It supports full read/write access to D64 and DFI and read-only access to images of a CMD HD.'),
+
+	'dload'		=> array(name=>'DLoad', 'path'=>'dload/index.php', 'dsc'=>
+'DreamLoad (or short DLoad) is a 2 bit IRQ fastloader for many different drives like 1541 and compatible,
+1570/71, 1581 and the CMD devices. The latest version also supports the MMC64.
+DLoad works works on pal and ntsc machines and can easily be combined with crunchers and levelpackers.'),
+
+	'dreamass'	=> array(name=>'DreamAss', 'path'=>'dreamass/index.php', 'dsc'=>
+'DreamAss is a crossassembler. It produces binaries for 6510, 65816 and compatible CPUs.
+The \'illegal\' opcodes of the 6510 are also supported.'),
+
+	'dreamon'	=> array(name=>'DreaMon', 'path'=>'dreamon/index.php', 'dsc'=>
+'DreaMon is an assembly language monitor for the SCPU. It requires a Super Ram Card. It supports Freezepoints and
+has a very comfortable and intuitive scroll mode for debugging. DreaMon can use the normal Vic display or on a C128
+the 80 columns screen with up to 50 lines (depends on the monitor)'),
+
+	'dreamon_crt'	=> array(name=>'DreaMon_crt', 'path'=>'dreamon_crt/index.php', 'dsc'=>
+'DreaMon_crt is a port of the DreaMon project to the Retro Replay cart.'),
+
+	'mmc64_plugin'	=> array(name=>'MMC64 Plugin', 'path'=>'mmc64_plugin/index.php', 'dsc'=>
+'This project provides a framework for MMC64 plugins. It contains several examples like a D64 and Lynx extractor and the DFI plugin.'),
+
+	'mylene'	=> array(name=>'Mylene', 'path'=>'mylene/index.php', 'dsc'=>
+'Mylene is a file browser library for the C64. It can be configured to display various information and supports caching.
+Mylene is used in the dfi and lnx plugins for the MMC64.')
+);
+
+
+$links = array(
+	'Retrohackers'	=> 'http://retrohackers.org',
+	'C64.sk'	=> 'http://www.c64.sk',
+	'CSDB'		=> 'http://noname.c64.org/csdb',
+	'The Dreams'	=> 'http://www.the-dreams.de'
+);
+
+
+// Copy all banner codes into this array, quoted with single ticks (').
+// Don't forget to escape any ticks in the banner code itself. :)
+$banners = array(
+
+	// Small "Defective By Design" badge from http://www.defectivebydesign.org/promo/button
+'<a href="http://www.defectivebydesign.org/join/button">
+<img src="banner/dbd_sm_btn.png"
+alt="DRM is DefectiveByDesign"
+width="88" height="30" border="0" align="middle" /><br />
+<small>Protect your freedom!</small></a>',
+
+	// BadVista logo from http://badvista.fsf.org/logos/BadVista_no_littering.png/view
+'<a href="http://www.badvista.org" target="new">
+<img src="banner/BadVista_no_littering.png" border="0"
+alt="BadVista.org: Stopping Vista adoption by promoting free software"></a>'
+);
+
+
+
 $ARGV = array();
 $BERLIOS_GROUP_ID = 7602;
+
+if( isset($_SERVER['HTTPS']) )
+{
+	$PROTO = 'https';
+}
+else
+{
+	$PROTO = 'http';
+}
 
 // merge all vars
 $ARGV = array_merge($HTTP_POST_VARS, $HTTP_GET_VARS);
@@ -15,18 +89,9 @@ function ShowPageMain()
 {
 	global $ARGV;
 	global $BERLIOS_GROUP_ID;
-
-	$subpages = array(
-				// area name	, page path
-				'home'		=> 'home/index.php',
-				'cbmimager'	=> 'cbmimager/index.php',
-				'dload'		=> 'dload/index.php',
-				'dreamass'	=> 'dreamass/index.php',
-				'dreamon'	=> 'dreamon/index.php',
-				'dreamon_crt'	=> 'dreamon_crt/index.php',
-				'mmc64_plugin'	=> 'mmc64_plugin/index.php',
-				'mylene'	=> 'mylene/index.php'
-			);
+	global $PROTO;
+	global $banners;
+	global $subpages;
 
 
 	// set default page if the frontpage does not exist
@@ -42,13 +107,17 @@ function ShowPageMain()
 	// get page for area
 	if( isset($subpages[$area]) )
 	{
-		$file = $subpages[$area];
+		$ar_attr = $subpages[$area];
 	}
 
 	// include page file
-	if( isset($file) && is_file($file) )
+	if( isset($ar_attr) && isset($ar_attr['path']) )
 	{
-		include $file;
+		$file = $ar_attr['path'];
+		if( is_file($file) )
+		{
+			include $file;
+		}
 	}
 
 	print("<html><head>");
@@ -62,32 +131,51 @@ function ShowPageMain()
 	print("<tr><td><img src=\"br0.gif\"></td><td background=\"br1.gif\"><img src=\"br1.gif\"></td><td><img src=\"br2.gif\"></td><td width=2><img src=\"blk2x2.gif\"></td><td width=6000><img src=\"blk2x2.gif\"></td></tr>\n");
 	print("<tr><td background=\"br3.gif\" width=8><img src=\"br3.gif\"></td><td bgcolor=\"#222222\" valign=top>\n");
 
-	print("<strong>Menu<strong><font size=-1><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=home\">Home</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"http://developer.berlios.de/projects/rrtools\">Project&nbsp;Page</a><br>\n");
-	printf("&nbsp;&nbsp;&nbsp;<a href=\"http://developer.berlios.de/project/showfiles.php?group_id=%d\">All&nbsp;Downloads</a><br>\n", $BERLIOS_GROUP_ID);
-	printf("&nbsp;&nbsp;&nbsp;<a href=\"http://developer.berlios.de/mail/?group_id=%d\">All&nbsp;Mailinglists</a><br>\n", $BERLIOS_GROUP_ID);
-	print("</font><p>");
+	print "<strong>Menu<strong><font size=-1><br>\n";
+	if( isset($subpages['home']) )
+	{
+		$home_attr = $subpages['home'];
+		print '&nbsp;&nbsp;&nbsp;<a href="index.php?area=home">';
+		print str_replace(' ', '&nbsp;', $home_attr['name']);
+		print "</a><br>\n";
+	}
+	print "&nbsp;&nbsp;&nbsp;<a href=\"http://developer.berlios.de/projects/rrtools\">Project&nbsp;Page</a><br>\n";
+	print "&nbsp;&nbsp;&nbsp;<a href=\"http://developer.berlios.de/project/showfiles.php?group_id=$BERLIOS_GROUP_ID\">All&nbsp;Downloads</a><br>\n";
+	print "&nbsp;&nbsp;&nbsp;<a href=\"http://developer.berlios.de/mail/?group_id=$BERLIOS_GROUP_ID\">All&nbsp;Mailinglists</a><br>\n";
+	print "</font><p>\n";
 
 	print("<strong>Projects<strong><font size=-1><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=cbmimager\">CBMImager</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=dload\">DLoad</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=dreamass\">DreaMon</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=dreamon\">DreaMon</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=dreamon_crt\">DreaMon_CRT</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=mmc64_plugin\">MMC64 Plugin</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"index.php?area=mylene\">Mylene</a><br>\n");
+	foreach($subpages as $section=>$ar_attr)
+	{
+		// is this the 'home' page?
+		if( $section!='home' )
+		{
+			print '&nbsp;&nbsp;&nbsp;<a href="index.php?area=';
+			print $section;
+			print '">';
+			print str_replace(' ', '&nbsp;', $ar_attr['name']);
+			print "</a><br>\n";
+		}
+	}
 	print("</font><p>");
 
 	print("<strong>Links<strong><font size=-1><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"http://retrohackers.org\">Retrohackers</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"http://www.c64.sk/\">C64.sk</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"http://noname.c64.org/csdb/\">CSDB</a><br>\n");
-	print("&nbsp;&nbsp;&nbsp;<a href=\"http://www.the-dreams.de/\">The&nbsp;Dreams</a><br>\n");
-	print("</font><p>");
+	ShowLinks('&nbsp;&nbsp;&nbsp;', "<br>\n");
+	print '</font><p><br>';
+
+	// show some banners
+	print '<center><font size=-2>';
+	foreach($banners as $code)
+	{
+		print $code;
+		print '<p>';
+	}
+	print '</font></center><p><br>';
 
 	// show berlios logo
-	ShowBerliOsLogo();
+	print "<center><font size=-2>Thanks to<br>\n";
+	print "<a href=\"$PROTO://developer.berlios.de\"><img src=\"$PROTO://developer.berlios.de/bslogo.php?group_id=$BERLIOS_GROUP_ID\" width=\"124\" height=\"32\" border=\"0\" alt=\"BerliOS Logo\"/></a><br>\n";
+	print("for hosting us!</font></center>\n");
 
 	print("</td><td background=\"br4.gif\" width=8><img src=\"br4.gif\"></td><td width=2><img src=\"blk2x2.gif\"></td>\n");
 	print("<td valign=top width=6000 ");
@@ -111,14 +199,21 @@ function ShowPageMain()
 }
 
 
-function ShowBerliOsLogo()
+function ShowLinks($strPre, $strPost)
 {
-	global $BERLIOS_GROUP_ID;
+	global $links;
 
 
-	print("<center><font size=-2>Thanks to<br>\n");
-	printf("<a href=\"http://developer.berlios.de\"><img src=\"http://developer.berlios.de/bslogo.php?group_id=%d\" width=\"124\" height=\"32\" border=\"0\" alt=\"BerliOS Logo\"/></a><br>\n", $BERLIOS_GROUP_ID);
-	print("for hosting us!</font></center>\n");
+	// show all links in
+	foreach($links as $link_name=>$link_url)
+	{
+		print $strPre;
+		print '<a href="';
+		print $link_url;
+		print '">';
+		print str_replace(' ', '&nbsp;', $link_name);
+		print '</a>';
+		print $strPost;
+	}
 }
-
 ?>
