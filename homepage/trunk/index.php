3a0ca81968aaee3b004e89e59d973b2b85addf0b
baccy_drm <baccy_drm@dce8e5b7-fb20-0410-bec9-edb67563d200>
2007-01-14 04:30:34 +0000

    updated dbd banner, new c64.sk banner, new funstuff areas

--- homepage/trunk/index.php
+++ homepage/trunk/index.php
@@ -4,10 +4,10 @@
 
 
 // area name, page path and page description
-// NOTE: the first entry in this array must be the main page. it is omitted in the projects listing
+// NOTE: if the description is not set, the entry is not shown in the projects list
 $subpages = array(
-	'home'		=> array(name=>'Home', 'path'=>'home/index.php', 'dsc'=>
-''),
+	// 'home' is special, it should only appear in the main menu
+	'home'		=> array(name=>'Home', 'path'=>'home/index.php'),
 
 	'cbmimager'	=> array(name=>'CBMImager', 'path'=>'cbmimager/index.php', 'dsc'=>
 'CBMImager is a tool to create and edit disk images for the Commodore C64 and compatible computers.
@@ -35,7 +35,12 @@ the 80 columns screen with up to 50 lines (depends on the monitor)'),
 
 	'mylene'	=> array(name=>'Mylene', 'path'=>'mylene/index.php', 'dsc'=>
 'Mylene is a file browser library for the C64. It can be configured to display various information and supports caching.
-Mylene is used in the dfi and lnx plugins for the MMC64.')
+Mylene is used in the dfi and lnx plugins for the MMC64.'),
+
+	// next are the hidden entries (the fun stuff)
+	'bughunt'	=> array(name=>'Baccy\'s Big Bughunt', 'path'=>'bughunt/index.php'),
+
+	'sodom'		=> array(name=>'Das Tier = 666', 'path'=>'sodom/index.php')
 );
 
 
@@ -50,12 +55,12 @@ $links = array(
 // Copy all banner codes into this array, quoted with single ticks (').
 // Don't forget to escape any ticks in the banner code itself. :)
 $banners = array(
+	// c64.sk
+'<a href="http://www.c64.sk"><img src="banner/c64sk90x30.gif" alt="C64.sk Logo" border="0" align="middle"><br><small>C64 News</small></a>',
 
 	// Small "Defective By Design" badge from http://www.defectivebydesign.org/promo/button
 '<a href="http://www.defectivebydesign.org/join/button">
-<img src="banner/dbd_sm_btn.png"
-alt="DRM is DefectiveByDesign"
-width="88" height="30" border="0" align="middle" /><br />
+<img src="banner/dbd_sm_btn.png" alt="DRM is DefectiveByDesign" border="0" align="middle" /><br />
 <small>Protect your freedom!</small></a>',
 
 	// BadVista logo from http://badvista.fsf.org/logos/BadVista_no_littering.png/view
@@ -120,13 +125,19 @@ function ShowPageMain()
 		}
 	}
 
-	print("<html><head>");
-	print("<meta name=\"Keywords\" content=\"rrtools,c64,retro replay,mmc64,rrnet\">");
-	print("<meta name=\"Description\" content=\"Retro Replay Tools Homepage\">");
-	print("<title>Retro Replay Tools</title>");
-	print("</head>");
-	print("<body bgcolor=\"#000000\" text=\"#eeeeee\" alink=\"#33ccff\" vlink=\"#3366ff\" link=\"#3333ff\">");
-	print("<font face=\"Arial,Helvetica\">\n");
+	print '<html><head>';
+	print '<meta name="Keywords" content="rrtools,c64,retro replay,mmc64,rrnet">';
+	print '<meta name="Description" content="Retro Replay Tools Homepage">';
+	print '<title>Retro Replay Tools';
+	if( isset($ar_attr) && isset($ar_attr['name']) )
+	{
+		print ' - ';
+		print $ar_attr['name'];
+	}
+	print '</title>';
+	print '</head>';
+	print '<body bgcolor="#000000" text="#eeeeee" alink="#33ccff" vlink="#3366ff" link="#3333ff">';
+	print "<font face=\"Arial,Helvetica\">\n";
 	print("<table border=0 cellpadding=0 cellspacing=0 width=100%><tbody>\n");
 	print("<tr><td><img src=\"br0.gif\"></td><td background=\"br1.gif\"><img src=\"br1.gif\"></td><td><img src=\"br2.gif\"></td><td width=2><img src=\"blk2x2.gif\"></td><td width=6000><img src=\"blk2x2.gif\"></td></tr>\n");
 	print("<tr><td background=\"br3.gif\" width=8><img src=\"br3.gif\"></td><td bgcolor=\"#222222\" valign=top>\n");
@@ -145,15 +156,15 @@ function ShowPageMain()
 	print "</font><p>\n";
 
 	print("<strong>Projects<strong><font size=-1><br>\n");
-	foreach($subpages as $section=>$ar_attr)
+	foreach($subpages as $section=>$prj_attr)
 	{
-		// is this the 'home' page?
-		if( $section!='home' )
+		// is the description set?
+		if( isset($prj_attr['dsc']) )
 		{
 			print '&nbsp;&nbsp;&nbsp;<a href="index.php?area=';
 			print $section;
 			print '">';
-			print str_replace(' ', '&nbsp;', $ar_attr['name']);
+			print str_replace(' ', '&nbsp;', $prj_attr['name']);
 			print "</a><br>\n";
 		}
 	}
@@ -172,9 +183,16 @@ function ShowPageMain()
 	}
 	print '</font></center><p><br>';
 
-	// show berlios logo
+	// show berlios logo, but not for local development
 	print "<center><font size=-2>Thanks to<br>\n";
-	print "<a href=\"$PROTO://developer.berlios.de\"><img src=\"$PROTO://developer.berlios.de/bslogo.php?group_id=$BERLIOS_GROUP_ID\" width=\"124\" height=\"32\" border=\"0\" alt=\"BerliOS Logo\"/></a><br>\n";
+	if( isset($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR']=="127.0.0.1" )
+	{
+		print 'BerliOS<br>';
+	}
+	else
+	{
+		print "<a href=\"$PROTO://developer.berlios.de\"><img src=\"$PROTO://developer.berlios.de/bslogo.php?group_id=$BERLIOS_GROUP_ID\" width=\"124\" height=\"32\" border=\"0\" alt=\"BerliOS Logo\"/></a><br>\n";
+	}
 	print("for hosting us!</font></center>\n");
 
 	print("</td><td background=\"br4.gif\" width=8><img src=\"br4.gif\"></td><td width=2><img src=\"blk2x2.gif\"></td>\n");
