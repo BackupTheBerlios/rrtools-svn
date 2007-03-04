6702dc760b5c610405b251560539ec6422a3ff3d
baccy_drm <baccy_drm@dce8e5b7-fb20-0410-bec9-edb67563d200>
2007-03-04 19:54:45 +0000

    added batchfiles to generate vc6 project from bakefile

---
+++ cbmimager/trunk/_genvc6_debug.bat
@@ -0,0 +1,11 @@
+echo off
+IF defined WXWIN (
+        bakefile -f msvc6prj -I%WXWIN%\build\bakefiles\wxpresets\presets -DBUILD=debug -o netx_usb netx_usb.bkl
+) ELSE (
+        echo *************
+        echo *** ERROR ***
+        echo *************
+        echo The wxwidgets environment variable 'WXWIN' is not set!
+        echo Please set this variable to the root of you wxwidgets installation,
+        echo e.g. c:\wxWidgets-2.6.2
+)
